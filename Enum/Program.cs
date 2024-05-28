00689f6c8d8df1feb2f3457a3ec6b2f152408a9f
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-29 02:09:57 +0600

    added

---
+++ Enum/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+    enum Months
+    {
+        January,    // 0
+        February,   // 1
+        March,      // 2
+        April,      // 3
+        May,        // 4
+        June,       // 5
+        July        // 6
+    }
+
+    enum Monthss
+    {
+        January,    // 0
+        February,   // 1
+        March = 6,    // 6
+        April,      // 7
+        May,        // 8
+        June,       // 9
+        July        // 10
+    }
+
+    static void Main(string[] args)
+    {
+        Level myVar = Level.Medium;
+        Console.WriteLine(myVar);
+        int myNum = (int)Months.April;
+        Console.WriteLine(myNum);
+        int myNums = (int)Monthss.April;
+        Console.WriteLine(myNums);
+
+        Level myVarr = Level.Medium;
+        switch (myVarr)
+        {
+            case Level.Low:
+                Console.WriteLine("Low level");
+                break;
+            case Level.Medium:
+                Console.WriteLine("Medium level");
+                break;
+            case Level.High:
+                Console.WriteLine("High level");
+                break;
+        }
+    }
+}
