1619f9a322b78381f3fcfd5ed7774eb833ebcbeb
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-28 23:00:35 +0600

    added class-OOPS

---
+++ OOP-Class/Program.cs
@@ -0,0 +1,12 @@
+﻿class Car
+{
+    string color = "red";
+
+    static void Main(string[] args)
+    {
+        Car myObj = new Car();
+        Car myObj2 = new Car();
+        Console.WriteLine(myObj.color);
+        Console.WriteLine(myObj2.color);
+    }
+}
