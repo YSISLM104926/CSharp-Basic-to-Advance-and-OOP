7583f5aa57ae3ef3d6974eb784a05adcee242f1d
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-28 22:42:48 +0600

    2 program added

--- HelloWorldProgram/Program.cs
+++ HelloWorldProgram/Program.cs
@@ -1,95 +1,106 @@
-﻿class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProgram
 {
-    public static void Main(string[] args)
+
+
+    class Program
     {
-        int myInt = 10;
-        double myDouble = 5.25;
-        bool myBool = true;
-
-        Console.WriteLine(Convert.ToString(myInt));    // convert int to string
-        Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
-        Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
-        Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
-                                                       // Type your username and press enter
-        Console.WriteLine("Enter username:");
-
-        // Create a string variable and get user input from the keyboard and store it in the variable
-        string userName = Console.ReadLine();
-
-        // Print the value of the variable (userName), which will display the input value
-        Console.WriteLine("Username is: " + userName);
-
-        string firstName = "John";
-        string lastName = "Doe";
-        string name = $"My full name is: {firstName} {lastName}";
-        Console.WriteLine(name);
-        int i = 0;
-        while (i < 5)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(i);
-            i++;
-        }
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
 
-        for (int k = 0; k <= 10; k = k + 2)
-        {
-            Console.WriteLine(k);
-        }
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+                                                           // Type your username and press enter
+            Console.WriteLine("Enter username:");
 
-        string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-        foreach (string f in cars)
-        {
-            Console.WriteLine(f);
-        }
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
 
-        int e = 0;
-        while (e < 10)
-        {
-            Console.WriteLine(e);
-            e++;
-            if (e == 4)
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+
+            string firstName = "John";
+            string lastName = "Doe";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
+            int i = 0;
+            while (i < 5)
             {
-                break;
+                Console.WriteLine(i);
+                i++;
             }
-        }
 
-        // Sort a string
-        string[] ca = { "Volvo", "BMW", "Ford", "Mazda" };
-        Array.Sort(ca);
-        foreach (string g in ca)
-        {
-            Console.WriteLine(g);
-        }
-
-
-        int[] myNumbers = { 5, 1, 8, 9 };
-        Console.WriteLine(myNumbers.Max());  // returns the largest value
-        Console.WriteLine(myNumbers.Min());  // returns the smallest value
-        Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
-
-        int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
-        Console.WriteLine(numbers[0, 2]);  // Outputs 2
-
-        int[,] number = { { 1, 4, 2 }, { 3, 6, 8 } };
-        numbers[0, 0] = 5;  // Change value to 5
-        Console.WriteLine(number[0, 0]); // Outputs 5 instead of 1
-
-
-        int[,] num = { { 1, 4, 2 }, { 3, 6, 8 } };
-
-        foreach (int ii in num)
-        {
-            Console.WriteLine(ii);
-        }
-
-        int[,] nu = { { 1, 4, 2 }, { 3, 6, 8 } };
-
-        for (int p = 0; p < nu.GetLength(0); p++)
-        {
-            for (int u = 0; u < nu.GetLength(1); u++)
+            for (int k = 0; k <= 10; k = k + 2)
             {
-                Console.WriteLine(nu[p, u]);
+                Console.WriteLine(k);
             }
-        }
 
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string f in cars)
+            {
+                Console.WriteLine(f);
+            }
+
+            int e = 0;
+            while (e < 10)
+            {
+                Console.WriteLine(e);
+                e++;
+                if (e == 4)
+                {
+                    break;
+                }
+            }
+
+            // Sort a string
+            string[] ca = { "Volvo", "BMW", "Ford", "Mazda" };
+            Array.Sort(ca);
+            foreach (string g in ca)
+            {
+                Console.WriteLine(g);
+            }
+
+
+            int[] myNumbers = { 5, 1, 8, 9 };
+            Console.WriteLine(myNumbers.Max());  // returns the largest value
+            Console.WriteLine(myNumbers.Min());  // returns the smallest value
+            Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
+
+            int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+            Console.WriteLine(numbers[0, 2]);  // Outputs 2
+
+            int[,] number = { { 1, 4, 2 }, { 3, 6, 8 } };
+            numbers[0, 0] = 5;  // Change value to 5
+            Console.WriteLine(number[0, 0]); // Outputs 5 instead of 1
+
+
+            int[,] num = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+            foreach (int ii in num)
+            {
+                Console.WriteLine(ii);
+            }
+
+            int[,] nu = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+            for (int p = 0; p < nu.GetLength(0); p++)
+            {
+                for (int u = 0; u < nu.GetLength(1); u++)
+                {
+                    Console.WriteLine(nu[p, u]);
+                }
+            }
+
+        }
     }
 }
---
+++ program2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProgram
+{
+    class program2
+    {
+        public static void Main(string[] args)
+        {
+            MyMethod();
+        }
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+    }
+}
