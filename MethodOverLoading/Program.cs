c044fd27b39976aacfdafa4aac43d3c13fa3e053
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-28 22:52:19 +0600

    added

---
+++ MethodOverLoading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProgram
+{
+    class DefaultParameter
+    {   
+        // overloading
+        static int PlusMethodInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethodDouble(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int myNum1 = PlusMethodInt(8, 5);
+            double myNum2 = PlusMethodDouble(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+        }
+    }
+}
