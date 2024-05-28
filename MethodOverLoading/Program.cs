b6695320691fbf841ffa564ed03aeee8c94d7b20
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-28 22:54:31 +0600

    added

--- MethodOverLoading/Program.cs
+++ MethodOverLoading/Program.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 namespace HelloWorldProgram
 {
     class DefaultParameter
-    {   
-        // overloading
-        static int PlusMethodInt(int x, int y)
+    {
+        // Method Overloading
+        static int PlusMethod(int x, int y)
         {
             return x + y;
         }
 
-        static double PlusMethodDouble(double x, double y)
+        static double PlusMethod(double x, double y)
         {
             return x + y;
         }
 
         static void Main(string[] args)
         {
-            int myNum1 = PlusMethodInt(8, 5);
-            double myNum2 = PlusMethodDouble(4.3, 6.26);
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
             Console.WriteLine("Int: " + myNum1);
             Console.WriteLine("Double: " + myNum2);
         }
